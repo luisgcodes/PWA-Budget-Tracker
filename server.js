c5a0179dd8180b56cc56770ca1afeb24d3a49d91
luisgcodes <lcienfuegos95@gmail.com>
2022-07-01 16:07:39 -0700

    readme

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const compression = require("compression");
 const PORT = process.env.PORT || 3001;
 const MONGODB_URI = process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/budget";
 
+
 const app = express();
 
 app.use(logger("dev"));
