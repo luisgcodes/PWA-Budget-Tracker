dcbad8b3d3da2b56fa4bb74b4c2d327835e5f941
luisgcodes <lcienfuegos95@gmail.com>
2022-07-02 20:29:49 -0700

    idb

---
+++ public/js/idb.js
@@ -0,0 +1,66 @@
+let db;
+const request = indexedDB.open('budget', 1);
+// name of database is budget
+
+request.onupgradeneeded = function(event) {
+  const db = event.target.result;
+  db.createObjectStore('new_transaction', { autoIncrement: true });
+};
+
+request.onsuccess = function(event) {
+  db = event.target.result;
+
+  if (navigator.onLine) {
+    uploadPizza();
+  }
+};
+
+request.onerror = function(event) {
+  console.log(event.target.errorCode);
+};
+
+function saveRecord(record) {
+  const transaction = db.transaction(['new_transaction'], 'readwrite');
+
+  const pizzaObjectStore = transaction.objectStore('new_transaction');
+
+  pizzaObjectStore.add(record);
+}
+
+function uploadPizza() {
+  const transaction = db.transaction(['new_transaction'], 'readwrite');
+
+
+  const pizzaObjectStore = transaction.objectStore('new_transaction');
+
+  const getAll = pizzaObjectStore.getAll();
+
+  getAll.onsuccess = function() {
+    if (getAll.result.length > 0) {
+      fetch('/api/transaction', {
+        method: 'POST',
+        body: JSON.stringify(getAll.result),
+        headers: {
+          Accept: 'application/json, text/plain, */*',
+          'Content-Type': 'application/json'
+        }
+      })
+        .then(response => response.json())
+        .then(serverResponse => {
+          if (serverResponse.message) {
+            throw new Error(serverResponse);
+          }
+
+          const transaction = db.transaction(['new_transaction'], 'readwrite');
+          const pizzaObjectStore = transaction.objectStore('new_transaction');
+          pizzaObjectStore.clear();
+        })
+        .catch(err => {
+       
+          console.log(err);
+        });
+    }
+  };
+}
+
+window.addEventListener('online', uploadPizza);
--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const PORT = process.env.PORT || 3001;
 const MONGODB_URI = process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/budget";
 
 
+
 const app = express();
 
 app.use(logger("dev"));
